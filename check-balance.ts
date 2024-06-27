133881f4f8fb3687c3c96f379dee242e989b146c
AdriGeorge <adrian7ko@gmail.com>
2024-06-27 20:05:33 +0300

    chore: check balance

---
+++ check-balance.ts
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js"
+import { airdropIfRequired, getKeypairFromEnvironment } from "@solana-developers/helpers"
+
+const connection = new Connection(clusterApiUrl("devnet"))
+
+console.log("Connected!")
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+const publickKey = keypair.publicKey
+
+await airdropIfRequired(connection, publickKey, LAMPORTS_PER_SOL, 0.1 * LAMPORTS_PER_SOL)
+
+const balanceInLamports = await connection.getBalance(publickKey)
+console.log(`Balance in lamports : ${balanceInLamports}`)
+const balance = balanceInLamports / LAMPORTS_PER_SOL
+console.log(`Balance in SOL: ${balance}`)
+
--- load-keypair.ts
+++ load-keypair.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import bs58 from "bs58"
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
@@ -7,3 +8,5 @@ console.log(
 `✅ Finished! We've loaded our keypair securely, using an env file! Our public key
 is: ${keypair.publicKey.toBase58()}`
 );
+
+// console.log(bs58.encode(keypair.secretKey));
